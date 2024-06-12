be74bea2c7d33593afec8a9b94713e284d292bfb
Paulaacebo <acebopaula@gmail.com>
2024-06-12 14:56:30 +0200

    commit prep_forecast_day

---
+++ models/prep/prep_forecast_day.sql
@@ -0,0 +1,16 @@
+WITH forecast_day_data AS (
+    SELECT * 
+    FROM staging_forecast_day
+),
+add_features AS (
+    SELECT *,
+        EXTRACT(DAY FROM date_column) AS day_of_month, -- day of month as a number
+        TO_CHAR(date_column, 'Month') AS month_of_year, -- month name as a text
+        EXTRACT(YEAR FROM date_column) AS year, -- year as a number
+        TO_CHAR(date_column, 'Day') AS day_of_week, -- weekday name as text
+        EXTRACT(WEEK FROM date_column) AS week_of_year, -- calendar week number as number
+        CONCAT(EXTRACT(YEAR FROM date_column), '-', EXTRACT(WEEK FROM date_column)) AS year_and_week -- year-calendarweek as text like '2024-43'
+    FROM forecast_day_data
+)
+SELECT *
+FROM add_features;
