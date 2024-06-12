23d1c443eeeb11e3120ed34f1aab9e3d4775ca1a
Paulaacebo <acebopaula@gmail.com>
2024-06-12 19:12:53 +0200

    add prep_forecast_hours

---
+++ models/prep/prep_forecast_hour.sql
@@ -0,0 +1,14 @@
+WITH forecast_hour_data AS (
+    SELECT * 
+    FROM {{ref('staging_forecast_hour')}}
+),
+add_features AS (
+    SELECT *
+        ,date_time::time AS time -- Extracting time (hours:minutes:seconds) as TIME data type
+        ,TO_CHAR(date_time, 'HH24:MI') AS hour -- Extracting time (hours:minutes) as TEXT data type
+        ,TO_CHAR(date_time, 'Month') AS month_of_year -- Extracting month name as text
+        ,TO_CHAR(date_time, 'Day') AS day_of_week -- Extracting weekday name as text
+    FROM forecast_hour_data
+)
+SELECT *
+FROM add_features
