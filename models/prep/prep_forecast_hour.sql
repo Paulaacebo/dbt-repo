4a539a992d4e810cb329883c18745fbbf8014ce1
Paulaacebo <acebopaula@gmail.com>
2024-06-12 19:18:51 +0200

    commit III prep_forecast_hour

--- models/prep/prep_forecast_hour.sql
+++ models/prep/prep_forecast_hour.sql
@@ -4,11 +4,12 @@ WITH forecast_hour_data AS (
 ),
 add_features AS (
     SELECT *
-        ,date::time AS time -- Extracting time (hours:minutes:seconds) as TIME data type
-        ,TO_CHAR(date, 'HH24:MI') AS hour -- Extracting time (hours:minutes) as TEXT data type
+        ,CAST(date AS TIMESTAMP) AS timestamp_with_default_time -- Convert date to timestamp (default time is 00:00:00)
+        ,EXTRACT(HOUR FROM CAST(date AS TIMESTAMP))::TEXT || ':' || LPAD(EXTRACT(MINUTE FROM CAST(date AS TIMESTAMP))::TEXT, 2, '0') AS hour -- Extracting time as TEXT data type
         ,TO_CHAR(date, 'Month') AS month_of_year -- Extracting month name as text
         ,TO_CHAR(date, 'Day') AS day_of_week -- Extracting weekday name as text
     FROM forecast_hour_data
 )
 SELECT *
-FROM add_features
+FROM add_features;
+
